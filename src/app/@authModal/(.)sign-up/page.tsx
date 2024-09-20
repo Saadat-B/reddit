43894c5cb23124f6eeca311d8197eabee42e23c1
Saadat-B <saadatbadgujar@gmail.com>
2024-09-20 11:41:44 +0530

    Fix modal close functionality

--- src/app/@authModal/(.)sign-up/page.tsx
+++ src/app/@authModal/(.)sign-up/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import CloseModal from "@/components/CloseModal";
-import SignIn from "@/components/SignIn";
+import SignUp from "@/components/SignUp";
 import { Dispatch, FC, SetStateAction, useState } from "react";
 
 interface pageProps {
@@ -9,15 +9,15 @@ interface pageProps {
 }
 
 const page: FC<pageProps> = ({}) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true); 
   return isOpen ? (
     <div className="fixed inset-0 bg-zinc-900/20 z-10">
       <div className="container flex items-center h-full max-w-lg mx-auto">
         <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
           <div className="absolute top-4 right-4">
-            <CloseModal goToHome={false} setIsOpen={setIsOpen} />
+            <CloseModal goToHome={true} setIsOpen={setIsOpen} />
           </div>
-          <SignIn />
+          <SignUp />
         </div>
       </div>
     </div>
--- src/components/CloseModal.tsx
+++ src/components/CloseModal.tsx
@@ -3,15 +3,37 @@
 import { X } from "lucide-react";
 import { Button } from "./ui/Button";
 import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useState } from "react";
 
-const CloseModal = () => {
+interface CloseModalProps {
+  goToHome?: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const CloseModal: React.FC<CloseModalProps> = ({
+  goToHome = false,
+  setIsOpen,
+}) => {
   const router = useRouter();
+
+  const handleClose = async () => {
+    setIsOpen(false); // Close the modal
+    if (typeof window !== "undefined") {
+      // Check if the code is running in the browser
+      if (goToHome) {
+        await router.push("/");
+      } else {
+        router.back();
+      }
+    }
+  };
+
   return (
     <Button
       variant="subtle"
       className="h-6 w-6 p-0 rounded-md"
       aria-label="close modal"
-      onClick={() => router.back()}
+      onClick={handleClose}
     >
       <X className="h-4 w-4" />
     </Button>
