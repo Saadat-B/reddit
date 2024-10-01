f86ceff775c2e667e3b3936ec060286d1f3ae5f6
Saadat-B <saadatbadgujar@gmail.com>
2024-10-01 14:25:52 +0530

    Update uploadthing package version and refactor code

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -105,12 +105,15 @@ export const Editor: React.FC<EditorProps> = ({ subredditId }) => {
               uploader: {
                 async uploadByFile(file: File) {
                   // upload to uploadthing
-                  const [res] = await uploadFiles([file], "imageUploader");
+
+                  const [res] = await uploadFiles("imageUploader", {
+                    files: [file],
+                  });
 
                   return {
                     success: 1,
                     file: {
-                      url: res.fileUrl,
+                      url: res.url,
                     },
                   };
                 },
--- src/lib/uploadthing.ts
+++ src/lib/uploadthing.ts
@@ -1,5 +1,13 @@
-import { generateReactHelpers } from "@uploadthing/react/hooks";
+import {
+  generateUploadButton,
+  generateUploadDropzone,
+} from "@uploadthing/react";
+
+import { generateReactHelpers } from "@uploadthing/react";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
 
 export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
